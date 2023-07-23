baa2cff5717d01313f986afed919c72211b52d1d
Gwinyai <gnyatsoka@gmail.com>
2023-07-24 01:55:35 +0200

    Added links to create account and signin viewcontrollers

--- ChatApp/ViewControllers/CreateAccountViewController.swift
+++ ChatApp/ViewControllers/CreateAccountViewController.swift
@@ -11,7 +11,7 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var createAccountTextView: UITextView!
+    @IBOutlet weak var signinAccountTextView: UITextView!
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var contentView: UIView!
     @IBOutlet weak var scrollView: UIScrollView!
@@ -20,6 +20,14 @@ class CreateAccountViewController: UIViewController {
         super.viewDidLoad()
         containerView.clipsToBounds = true
         containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        let attributedString = NSMutableAttributedString(string: "Already have an account? Sign in here.", attributes: [.font: Font.caption])
+        attributedString.addAttribute(.link, value: "chatappcreate://createAccount", range: (attributedString.string as NSString).range(of: "Sign in here."))
+        signinAccountTextView.attributedText = attributedString
+        signinAccountTextView.linkTextAttributes = [.foregroundColor: UIColor.secondary, .font: Font.linkLabel]
+        signinAccountTextView.delegate = self
+        signinAccountTextView.isScrollEnabled = false
+        signinAccountTextView.textAlignment = .center
+        signinAccountTextView.isEditable = false
     }
     
     override func viewDidLayoutSubviews() {
@@ -31,4 +39,15 @@ class CreateAccountViewController: UIViewController {
     }
     
 
+}
+
+extension CreateAccountViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        if URL.scheme == "chatappcreate" {
+            performSegue(withIdentifier: "SignInSegue", sender: nil)
+        }
+        return false
+    }
+    
 }
