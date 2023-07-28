bb7f5f4d241a56b2fc93d00f612acbb370323d81
Gwinyai <gnyatsoka@gmail.com>
2023-07-29 01:10:19 +0200

    Refactored SigninViewController

--- ChatApp/ViewControllers/SignInViewController.swift
+++ ChatApp/ViewControllers/SignInViewController.swift
@@ -87,8 +87,24 @@ class SignInViewController: UIViewController {
             return
         }
         showLoadingView()
+        signinUser(email: email, password: password) { [weak self] success, error in
+            guard let strongSelf = self else { return }
+            if let error = error {
+                print(error)
+                strongSelf.presentErrorAlert(title: "Signin Error", message: error)
+                return
+            }
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeVC = mainStoryboard.instantiateViewController(withIdentifier: "HomeViewController")
+            let navVC = UINavigationController(rootViewController: homeVC)
+            let window = UIApplication.shared.connectedScenes.flatMap { ($0 as? UIWindowScene)?.windows ?? [] }.first { $0.isKeyWindow }
+            window?.rootViewController = navVC
+        }
+    }
+    
+    func signinUser(email: String, password: String, completion: @escaping (_ success: Bool, _ error: String?) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { _, error in
-            self.removeLoadingView()
+            //self.removeLoadingView()
             if let error = error {
                 print(error.localizedDescription)
                 var errorMessage = "Something went wrong. Please try again later."
@@ -102,14 +118,10 @@ class SignInViewController: UIViewController {
                         break
                     }
                 }
-                self.presentErrorAlert(title: "Create Account Failed", message: errorMessage)
+                completion(false, errorMessage)
                 return
             }
-            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let homeVC = mainStoryboard.instantiateViewController(withIdentifier: "HomeViewController")
-            let navVC = UINavigationController(rootViewController: homeVC)
-            let window = UIApplication.shared.connectedScenes.flatMap { ($0 as? UIWindowScene)?.windows ?? [] }.first { $0.isKeyWindow }
-            window?.rootViewController = navVC
+            completion(true, nil)
         }
     }
 
